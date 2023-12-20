d3d29332ce67e8a72116d8af42fc672815dd71de
murrrda <aleksamrda@gmail.com>
2023-12-20 02:27:39 +0100

    Day 2

--- day1a/target/debug/deps/day1-d4a47a0ee2e67595.d
+++ day1a/target/debug/deps/day1-d4a47a0ee2e67595.d
@@ -1,5 +1,5 @@
-/home/mrda/Documents/aoc23/day1/target/debug/deps/libday1-d4a47a0ee2e67595.rmeta: src/main.rs
+/home/mrda/Documents/aoc23/day1a/target/debug/deps/libday1-d4a47a0ee2e67595.rmeta: src/main.rs
 
-/home/mrda/Documents/aoc23/day1/target/debug/deps/day1-d4a47a0ee2e67595.d: src/main.rs
+/home/mrda/Documents/aoc23/day1a/target/debug/deps/day1-d4a47a0ee2e67595.d: src/main.rs
 
 src/main.rs:
